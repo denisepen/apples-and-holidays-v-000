1549e8ae050be82464af4af4bc8b65a750b54529
Denise Pendleton <dpendl1@hotmail.com>
2018-02-23 02:18:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    holiday_hash
+    puts holiday_hash
   end
 
 end
