df85a86e86b793c63b46420392666fc9a2809d37
Denise Pendleton <dpendl1@hotmail.com>
2018-02-23 02:41:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,6 +91,6 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.map do |holiday_name, supplies|
       supplies.include?("BBQ") ? holiday_name : nil
     end
-  end
+  end.flatten.compact
 
 end
