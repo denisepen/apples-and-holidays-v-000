4227800194aac2c398822162db3695123cfd3f7e
Denise Pendleton <dpendl1@hotmail.com>
2018-02-23 02:21:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |key, value|
+  holiday_hash.each do |season, holiday|
     puts holiday_hash
   end
 
